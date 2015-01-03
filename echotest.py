6b4d1d777a361418f8a5ca65e3b19b4c86040e4d
Aeneas42 <danausetcorvus@gmail.com>
2015-01-02 21:04:15 -0700

    minor changes in comments

---
+++ echotest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# project         :mutagen-tutorial
+# title           :echotest.py
+# description     :
+# author          :ravila
+# date            :January 02, 2015
+# version         :
+# usage           :
+# notes           :
+# python_version  :3.4
+# ==============================================================================
+
